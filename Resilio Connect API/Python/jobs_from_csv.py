3b4fe18746e8b9124b8bd3adb31f5777b336ca53
Ilan Shamir <ilan@resilio.com>
2020-11-01 22:43:05 -0800

    jobs from csv checks the current state of the MC and add monitors for existing jobs. Allows for better recovery from stopping and restarting the python app

--- Resilio Connect API/Python/jobs_from_csv.py
+++ Resilio Connect API/Python/jobs_from_csv.py
@@ -2,7 +2,7 @@ import csv
 import time
 import threading
 
-from jobs import addSimpleSyncJob, getJobs
+from jobs import addSimpleSyncJob, getJobs, getJobsByAttrs, getJobRunID
 
 class jobsFromCSV:
     def __init__(self, csvFile, jobsMonitor, callbackFunction, concurrentJobsCount, monitorInterval):
@@ -12,12 +12,14 @@ class jobsFromCSV:
         self.callbackFunction = callbackFunction
         self.concurrentJobsCount = concurrentJobsCount
         self.monitorInterval = monitorInterval
-        threading.Thread(target=self.jobCreationThread).start()
         with open(csvFile, "r") as file:
             csvReader = csv.DictReader(file, skipinitialspace=True)
             for row in csvReader:
+                row['sentForExecution'] = False         # use this to track if the job was already sent for execution
                 self.jobsList.append(row)    
             print(self.jobsList)
+        self.syncCurrentState()
+        threading.Thread(target=self.jobCreationThread).start()
 
     def addNextJob(self):
         if (self.nextJob < len(self.jobsList)):
@@ -35,12 +37,34 @@ class jobsFromCSV:
                 self.jobsList[self.nextJob]['folder2'],
                 self.jobsMonitor
                 )
+            self.jobsList[self.nextJob]['sentForExecution'] = True
             self.nextJob += 1
+
+    def syncCurrentState(self):
+        for index in range(len(self.jobsList)):
+            jobExists = getJobsByAttrs("name", self.jobsList[index]['jobName'])
+            if (jobExists):
+                # TO DO: log it
+                self.jobsList[index]['sentForExecution'] = True
+                jobRunID = getJobRunID(jobExists[0]['id'])
+                self.jobsMonitor.addMonitoredJob(jobRunID['data'][0]['id'], self.callbackFunction)
 
     def jobCreationThread(self):
         while (True):
             jobs = getJobs()
             while ((self.nextJob < len(self.jobsList)) and (len(jobs) < self.concurrentJobsCount)):
-                self.addNextJob()
-                jobs = getJobs()
+                jobExists = getJobsByAttrs("name", self.jobsList[self.nextJob]['jobName'])
+                # was it executed before?
+                if (self.jobsList[self.nextJob]['sentForExecution']):
+                    # TO DO: log it
+                    self.nextJob += 1
+                elif (not jobExists):
+                    self.addNextJob()
+                    jobs = getJobs()
+                else:
+                    # TO DO: log it
+                    self.jobsList[self.nextJob]['sentForExecution'] = True
+                    jobRunID = getJobRunID(jobExists[0]['id'])
+                    self.jobsMonitor.addMonitoredJob(jobRunID['data'][0]['id'], self.callbackFunction)
+                    self.nextJob += 1
             time.sleep(self.monitorInterval)
